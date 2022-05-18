cd3c737ac25165b809b53f25854ec48b2da98dcd
Rolando Arellano <80436392+rolandoarellano69@users.noreply.github.com>
2022-05-17 22:16:32 -0700

    Update Practice1-U3.r

--- Practice1-U3.r
+++ Practice1-U3.r
@@ -4,24 +4,23 @@
 ```r
 Importing the dataset
 dataset <- read.csv(file.choose())
-//Splitting the dataset into the Training set and Test set
+##Splitting the dataset into the Training set and Test set
 Install.packages('caTools')
 library(caTools)
 set.seed(123)
 split <- sample.split(dataset$Salary, SplitRatio = 2/3)
 training_set <- subset(dataset, split == TRUE)
 test_set <- subset(dataset, split == FALSE)
-```
-```r
-//Fitting Simple Linear Regression to the Training set
+
+##Fitting Simple Linear Regression to the Training set
 regressor = lm(formula = Salary ~ YearsExperience,
                data = dataset)
 summary(regressor)
 
 # Predicting the Test set results
 y_pred = predict(regressor, newdata = test_set)
-```
-//Visualising the Training set results
+
+##Visualising the Training set results
 library(ggplot2)
 ggplot() +
   geom_point(aes(x=training_set$YearsExperience, y=training_set$Salary),
@@ -31,8 +30,8 @@ ggplot() +
   ggtitle('Salary vs Experience (Training Set)') +
   xlab('Years of experience') +
   ylab('Salary')
-```r
-// Visualising the Test set results
+
+##Visualising the Test set results
 ggplot() +
   geom_point(aes(x=test_set$YearsExperience, y=test_set$Salary),
              color = 'red') +
